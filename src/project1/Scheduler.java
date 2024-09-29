ec98d00b4dcfaaad43ed1ba3d53ca5c19c3f4b88
Aidan Pembleton <8npemb@gmail.com>
2024-09-29 19:11:05 -0400

    Additions to Scheduler for directing commands

--- src/project1/Scheduler.java
+++ src/project1/Scheduler.java
@@ -14,15 +14,55 @@ public class Scheduler {
         while (true) {
             String input = scanner.nextLine().trim();
 
-            if (input.toLowerCase().equals("Q")) {
+            if (input.isEmpty()) System.out.println("Please provide an input.");
+
+            if (input.charAt(0) == 'Q') {
                 System.out.println("Scheduler terminated.");
                 break;
             }
 
-            //code to process command follows
+            processCommand(input);
         }
 
         scanner.close();
+    }
+
+    // Direct commands to respective functions
+    private void processCommand(String input) {
+        String command = getCommand(input);
+        switch (command) {
+            case "S":
+                System.out.println("Schedule");
+                break;
+            case "C":
+                System.out.println("C");
+                break;
+            case "R":
+                System.out.println("R");
+                break;
+            case "PA":
+                System.out.println("PA");
+                break;
+            case "PP":
+                System.out.println("PP");
+                break;
+            case "PL":
+                System.out.println("PL");
+                break;
+            case "PS":
+                System.out.println("PS");
+                break;
+            default:
+                System.out.println("Invalid command.");
+                break;
+
+            }
+
+    }
+
+    private String getCommand(String input) {
+        int commaIndex = input.indexOf(',');
+        return input.substring(0, commaIndex);
     }
 
 }
