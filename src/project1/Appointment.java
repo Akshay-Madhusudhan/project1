004192e49ae54793f47eff3488cac259d2563317
Akshay_Madhusudhan <am3197@scarletmail.rutgers.edu>
2024-09-29 19:42:07 -0400

    Implemented equals in Appointment

--- src/project1/Appointment.java
+++ src/project1/Appointment.java
@@ -1,12 +1,25 @@
 package project1;
-import java.util.Calendar;
-import java.util.Date;
 
 public class Appointment implements Comparable<Appointment>{
     private Date date;
     private Timeslot timeslot;
     private Profile patient;
     private Provider provider;
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj==null){
+            return false;
+        }
+        if(obj.getClass() != this.getClass()){
+            return false;
+        }
+        Appointment app = (Appointment)obj;
+        if(app.date.equals(this.date) && app.timeslot.equals(this.timeslot) && app.patient.equals(this.patient)){
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public int compareTo(Appointment appointment){
